bff1be51d7235f7279bba2eac9c8b44b7f285fd7
VeroPolegre <veronicapolegre98@gmail.com>
2023-10-31 13:26:51 +0100

    bcrypt model

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,13 +1,31 @@
 const { User } = require("../models/index.js");
+const bcrypt = require("bcryptjs");
 
 const UserController = {
   create(req, res) {
     req.body.role = "user";
-    User.create(req.body)
+    const password = bcrypt.hashSync(req.body.password, 10);
+    User.create({ ...req.body, password })
       .then((user) =>
         res.status(201).send({ message: "User created succesfully!", user })
       )
       .catch((err) => console.error(err));
+  },
+  login(req, res) {
+    User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    }).then((user) => {
+      if (!user) {
+        return res.status(400).send({ message: "Incorrect user or password" });
+      }
+      const isMatch = bcrypt.compareSync(req.body.password, user.password);
+      if (!isMatch) {
+        return res.status(400).send({ message: "Incorrect user or password" });
+      }
+      res.send(user);
+    });
   },
 };
 
---
+++ migrations/20231031122232-changeUserColumn.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.changeColumn("Users", "email", {
+      type: Sequelize.STRING,
+      unique: true,
+    });
+  },
+  async down(queryInterface, Sequelize) {},
+};
