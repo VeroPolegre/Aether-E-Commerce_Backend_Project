de705dcb8113119721f8e30a491f89933c6eee4e
VeroPolegre <veronicapolegre98@gmail.com>
2023-11-01 14:52:03 +0100

    authentication middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,27 @@
+const { User, Token, Sequelize } = require("../models");
+const { Op } = Sequelize;
+const jwt = require("jsonwebtoken");
+const { jwt_secret } = require("../config/config.json")["development"];
+
+const authentication = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const payload = jwt.verify(token, jwt_secret);
+    const user = await User.findByPk(payload.id);
+    const tokenFound = await Token.findOne({
+      where: {
+        [Op.and]: [{ UserId: user.id }, { token: token }],
+      },
+    });
+    if (!tokenFound) {
+      return res.status(401).send({ message: "Not authorized" });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error, message: "Invalid token" });
+  }
+};
+
+module.exports = { authentication };
