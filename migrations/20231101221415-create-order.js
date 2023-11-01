1291c2df34588b33064051f732c0fba9e26e8efe
VeroPolegre <veronicapolegre98@gmail.com>
2023-11-01 23:18:54 +0100

    order model and migration

---
+++ migrations/20231101221415-create-order.js
@@ -0,0 +1,29 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Orders", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      date: Sequelize.DATE,
+      total: Sequelize.DECIMAL,
+      userId: Sequelize.UUID,
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Orders");
+  },
+};
---
+++ models/order.js
@@ -0,0 +1,27 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Order extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+  Order.init(
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      date: DataTypes.DATE,
+      total: DataTypes.DECIMAL,
+      userId: DataTypes.UUID,
+    },
+    {
+      sequelize,
+      modelName: "Order",
+    }
+  );
+  return Order;
+};
