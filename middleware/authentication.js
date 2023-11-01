a0efafa8da6ec5b4adbf3c4794e6ccdd7716d270
VeroPolegre <veronicapolegre98@gmail.com>
2023-11-01 20:33:17 +0100

    isAdmin authentication

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -24,4 +24,13 @@ const authentication = async (req, res, next) => {
   }
 };
 
-module.exports = { authentication };
+const isAdmin = async (req, res, next) => {
+  const admins = ["admin", "superadmin"];
+  if (!admins.includes(req.user.role)) {
+    return res.status(403).send({
+      message: "You do not have Administrator access",
+    });
+  }
+  next();
+};
+module.exports = { authentication, isAdmin };
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const router = express.Router();
-// const { authentication, isAdmin } = require("../middlewares/authentication");
+const { authentication, isAdmin } = require("../middleware/authentication");
 const UserController = require("../controllers/UserController");
 
 router.post("/", UserController.create);
+router.put(
+  "/:userId/changeRoleToAdmin",
+  authentication,
+  isAdmin,
+  UserController.changeRoleToAdmin
+);
+router.put(
+  "/:userId/changeRoleToUser",
+  authentication,
+  isAdmin,
+  UserController.changeRoleToUser
+);
+// router.get('/',authentication, UserController.getAll)
+// router.delete("/id/:id", authentication, isAdmin, UserController.delete);
+// router.put('/:id', authentication, UserController.update)
 router.post("/login", UserController.login);
-// router.post("/changeRole", authentication, isAdmin, UserController.changeRole);
-router.put("/:userId/changeRoleToAdmin", UserController.changeRoleToAdmin);
-router.put("/:userId/changeRoleToUser", UserController.changeRoleToUser);
+//router.delete("/logout", authentication, UserController.logout);
 module.exports = router;
