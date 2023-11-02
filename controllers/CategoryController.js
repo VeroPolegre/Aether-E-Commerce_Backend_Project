24077e2ac0f3cbbe8b7f436b86ec1ba110a19c82
VeroPolegre <veronicapolegre98@gmail.com>
2023-11-02 21:50:30 +0100

    create category endpoint

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -4,9 +4,9 @@ const CategoryController = {
   create(req, res) {
     Category.create(req.body)
       .then(
-        res
-          .status(201)
-          .send({ message: "Category created succesfully!", category })
+        res.status(201).send({
+          message: `Category '${req.body.name}' created succesfully!`,
+        })
       )
       .catch((err) => console.error(err));
   },
