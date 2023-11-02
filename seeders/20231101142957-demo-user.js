417ba264ae4e9e85e63b83187ff78f749b381d67
VeroPolegre <veronicapolegre98@gmail.com>
2023-11-02 22:05:57 +0100

    installed uuid dependency for user seeder, 5 users 1 admin

--- seeders/20231101142957-demo-user.js
+++ seeders/20231101142957-demo-user.js
@@ -1,15 +1,66 @@
 const bcrypt = require("bcryptjs");
+const { v4: uuidv4 } = require("uuid");
 ("use strict");
 module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert("Users", [
       {
-        id: 1,
+        id: uuidv4(),
         name: "Vero",
         email: "vero@example.com",
         password: bcrypt.hashSync("adminadmin", 10),
         avatar: "https://example.com/avatar1.jpg",
         role: "admin",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: "User1",
+        email: "user1@example.com",
+        password: bcrypt.hashSync("userpassword1", 10),
+        avatar: "https://example.com/avatar2.jpg",
+        role: "user",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: "User2",
+        email: "user2@example.com",
+        password: bcrypt.hashSync("userpassword2", 10),
+        avatar: "https://example.com/avatar3.jpg",
+        role: "user",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: "User3",
+        email: "user3@example.com",
+        password: bcrypt.hashSync("userpassword3", 10),
+        avatar: "https://example.com/avatar4.jpg",
+        role: "user",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: "User4",
+        email: "user4@example.com",
+        password: bcrypt.hashSync("userpassword4", 10),
+        avatar: "https://example.com/avatar5.jpg",
+        role: "user",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: "User5",
+        email: "user5@example.com",
+        password: bcrypt.hashSync("userpassword5", 10),
+        avatar: "https://example.com/avatar6.jpg",
+        role: "user",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
