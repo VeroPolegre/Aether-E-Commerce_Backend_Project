b012f77de10738ed65f9d206257f17914bcac978
VeroPolegre <veronicapolegre98@gmail.com>
2023-11-03 18:06:43 +0100

    authentication and admin added to games routes

--- routes/games.js
+++ routes/games.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
-
+const { authentication, isAdmin } = require("../middleware/authentication");
 const GameController = require("../controllers/GameController");
 
-router.post("/", GameController.create);
-router.put("/id/:id", GameController.update);
-router.delete("/id/:id", GameController.delete);
-router.get("/", GameController.getAll);
-router.get("/id/:id", GameController.getById);
-router.get("/title/:title", GameController.getByTitle);
-router.get("/price/:price", GameController.getByPrice);
-router.get("/desc-price", GameController.getByDescendingPrice);
+router.post("/", authentication, isAdmin, GameController.create);
+router.put("/id/:id", authentication, isAdmin, GameController.update);
+router.delete("/id/:id", authentication, isAdmin, GameController.delete);
+router.get("/", authentication, GameController.getAll);
+router.get("/id/:id", authentication, GameController.getById);
+router.get("/title/:title", authentication, GameController.getByTitle);
+router.get("/price/:price", authentication, GameController.getByPrice);
+router.get("/desc-price", authentication, GameController.getByDescendingPrice);
 module.exports = router;
