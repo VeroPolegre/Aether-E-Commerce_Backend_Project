d4991fb03352e7066abd8d59840a37231c33c1df
VeroPolegre <veronicapolegre98@gmail.com>
2023-11-03 16:35:39 +0100

    update game endpoint

--- controllers/GameController.js
+++ controllers/GameController.js
@@ -14,6 +14,22 @@ const GameController = {
       res.status(500).send({ msg: "Error creating a game", err });
     }
   },
+
+  async update(req, res) {
+    try {
+      const updatedGame = await Game.update(req.body, {
+        where: { id: req.params.id },
+      });
+      updatedGame[0] === 1
+        ? res
+            .status(200)
+            .send({ msg: `Game '${req.body.title}' updated succesfully!` })
+        : res.status(404).send({ msg: `Game not found` });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ msg: "Error updating a game", err });
+    }
+  },
 };
 
 module.exports = GameController;
--- routes/games.js
+++ routes/games.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const GameController = require("../controllers/GameController");
 
 router.post("/", GameController.create);
+router.put("/id/:id", GameController.update);
 
 module.exports = router;
