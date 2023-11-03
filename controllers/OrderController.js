d482485502307df503fffa992f0f2eb61118e4fa
VeroPolegre <veronicapolegre98@gmail.com>
2023-11-03 02:14:29 +0100

    create and get all orders endpoints

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -1,13 +1,26 @@
-const { Order, User } = require("../models/index.js");
+const { Order, Game } = require("../models/index.js");
 
 const OrderController = {
+  create(req, res) {
+    Order.create(req.body)
+      .then((order) => {
+        order.addGame(req.body.GameId);
+        res.status(200).send({ message: "Order placed successfully!", order });
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send({
+          message: "Problem creating the order",
+        });
+      });
+  },
   getAll(req, res) {
     Order.findAll({
-      include: [User],
+      include: [Game],
     })
       .then((orders) => res.send(orders))
-      .catch((err) => {
-        console.log(err);
+      .catch((error) => {
+        console.error(error);
         res.status(500).send({
           message: "Problem retrieving orders",
         });
--- routes/orders.js
+++ routes/orders.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const OrderController = require("../controllers/OrderController.js");
 
-// router.post("/create", OrderController.create);
-// router.get("/", OrderController.getAll);
+router.post("/", OrderController.create);
+router.get("/", OrderController.getAll);
 module.exports = router;
