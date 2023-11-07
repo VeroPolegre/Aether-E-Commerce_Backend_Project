497005d042568c7210f3d0d260a1f3517fe2ff4a
VeroPolegre <veronicapolegre98@gmail.com>
2023-11-07 19:39:13 +0100

    confirmed column forr users seeder

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { authentication, isAdmin } = require("../middleware/authentication");
 const ReviewController = require("../controllers/ReviewController.js");
 
-router.post("/", authentication, isAdmin, ReviewController.create);
+router.post("/", authentication, ReviewController.create);
 router.get("/", authentication, ReviewController.getAll);
 
 module.exports = router;
